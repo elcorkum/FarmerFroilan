1ee4e6bb4fcf5ed7ded72056e603a34384c1fd1f
elcorkum <lizzycorkum@gmail.com>
2024-04-18 12:28:47 -0400

    Update overriden methods in Pilot, Eater, Rider

--- src/main/java/mcfroilan/Pilot.java
+++ src/main/java/mcfroilan/Pilot.java
@@ -2,4 +2,18 @@ package mcfroilan;
 
 
 public class Pilot extends Person {
+    @Override
+    public void eat(Edible edible) {
+        System.out.println("Pilot is eating " + edible);
+    }
+
+    @Override
+    public void mount(Rideable rideable) {
+        System.out.println("Getting on the rideable");
+    }
+
+    @Override
+    public void dismount(Rideable rideable) {
+        System.out.println("Getting off the rideable");
+    }
 }
