8bd87cb67033d2155eb1733b74f4bfedf6d1124c
KalilK13 <kalilkennedy13@gmail.com>
2024-04-18 12:47:08 -0400

    modified Horse Chicken and Animal classes and NoiseMaker interface

--- src/main/java/mcfroilan/Chicken.java
+++ src/main/java/mcfroilan/Chicken.java
@@ -2,4 +2,34 @@ package mcfroilan;
 
 
 public class Chicken extends Animal implements Produce{
+    //private EdibleEgg egg;
+    private boolean hasBeenFertilized;
+
+    public Chicken(boolean isFretilized){
+        setHasBeenFertilized(isFretilized);
+    }
+
+    public boolean isHasBeenFertilized() {
+        return hasBeenFertilized;
+    }
+
+    public void setHasBeenFertilized(boolean hasBeenFertilized) {
+        this.hasBeenFertilized = hasBeenFertilized;
+    }
+    public Edible yield(){
+        if(isHasBeenFertilized() == true){
+            return new EdibleEgg();
+        }
+        return null;
+    }
+
+    @Override
+    public void eat(Edible edible) {
+
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("bok bok");
+    }
 }
--- src/main/java/mcfroilan/Horse.java
+++ src/main/java/mcfroilan/Horse.java
@@ -1,3 +1,12 @@
 package mcfroilan;
 public class Horse extends Animal implements Rideable{
+    @Override
+    public void eat(Edible edible) {
+
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Neigh!");
+    }
 }
