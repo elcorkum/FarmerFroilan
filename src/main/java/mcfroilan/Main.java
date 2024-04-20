90855da2bef405d420c903a45683785dcdcc5748
KalilK13 <kalilkennedy13@gmail.com>
2024-04-20 18:32:46 -0400

    added main class.

--- src/main/java/mcfroilan/Main.java
+++ src/main/java/mcfroilan/Main.java
@@ -4,8 +4,21 @@ import java.util.Arrays;
 
 public class Main{
     public static void main(String[] args) {
+        Farmer tim = new Farmer();
+        CropRow corn = new CropRow(3);
+        CornStalk cornStalk = new CornStalk();
+        tim.plant(cornStalk, corn, -1);
+        System.out.println(Arrays.toString(corn.getCrops()));
         Farm farm = new Farm();
        Chicken[][] chickenCoops = farm.makeChickens();
+       Tractor tractor = new Tractor();
+       Horse horse = new Horse();
+       CropDuster cropDuster = new CropDuster();
+       tim.mount(tractor);
+       tim.dismount();
+       //tim.mount(tractor);
+       //tim.mount(horse);
+
         //System.out.println(Arrays.deepToString(farm.makeChickens()));
         Chicken chicken1 = new Chicken();
         Chicken chicken2 = new Chicken();
