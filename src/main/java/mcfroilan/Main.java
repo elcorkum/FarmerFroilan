4dbfb374f16e615085693a2fb05fc4343b848b4e
elcorkum <lizzycorkum@gmail.com>
2024-04-19 20:00:01 -0400

    Add logic to operate() -Tractor and yield() -Chicken

--- src/main/java/mcfroilan/Main.java
+++ src/main/java/mcfroilan/Main.java
@@ -6,13 +6,7 @@ public class Main{
     public static void main(String[] args) {
         Farm farm = new Farm();
        Chicken[][] chickenCoops = farm.makeChickens();
-        //System.out.println(Arrays.deepToString(farm.makeChickens()));
-        Chicken chicken1 = new Chicken();
-        Chicken chicken2 = new Chicken();
-        Chicken chicken3 = new Chicken();
-        System.out.println(chicken1.getId());
-        System.out.println(chicken2.getId());
-        System.out.println(chicken3.getId());
+        farm.makeChickens();
         for(Chicken[] chickenCoop: chickenCoops) {
             for(Chicken chicken: chickenCoop){
                 if(chicken != null && chicken.getId() == 13){
