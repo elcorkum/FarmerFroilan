5dadffbcce01e891e1108b28594f6322f2200166
JuanL <laravega.juan8@gmail.com>
2024-04-21 11:38:38 -0400

    updated makeHorse method

--- src/main/java/mcfroilan/Farm.java
+++ src/main/java/mcfroilan/Farm.java
@@ -3,17 +3,17 @@ package mcfroilan;
 
 import java.util.Arrays;
 
-public class Farm{
-    private Stable[] stables;
+public class Farm {
+    private Stable[][] stables;
     private Chicken[][] chickenCoops;
     private FarmHouse farmHouse;
 
 
-    public Chicken[][] makeChickens(){
+    public Chicken[][] makeChickens() {
         chickenCoops = new Chicken[4][4];
-        for (int x = 0; x < 4; x++){
-            for (int y = 0; y < 4; y++){
-                if (x == 3 && y > 2){
+        for (int x = 0; x < 4; x++) {
+            for (int y = 0; y < 4; y++) {
+                if (x == 3 && y > 2) {
                     break;
                 }
                 chickenCoops[x][y] = new Chicken();
@@ -31,24 +31,17 @@ public class Farm{
                 "chickenCoops=" + Arrays.deepToString(chickenCoops) +
                 '}';
     }
-    public void makeHorses(int numHorses, int numStables){
-        int horses;
-        int stable;
 
-        Stable[] stables = new Stable[numStables];
-
-        int horsesPerStable = numHorses / numStables;
-
-        int remainingHorses = numHorses;
-        for (int i = 0; i < numStables; i++) {
-            int horsesForThisStable;
-            if (i == numStables - 1) {
-                horsesForThisStable = remainingHorses;
-            } else {
-                horsesForThisStable = horsesPerStable;
+    public Horse[][] makeHorses() {
+        Horse[][] stables = new Horse[3][4];
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 4; y++) {
+                if (x == 2 && y > 1) {
+                    break;
+                }
+                stables[x][y] = new Horse();
             }
-            System.out.println("Stable " + (i + 1) + ": " + horsesForThisStable + " horses");
-            remainingHorses -= horsesForThisStable;
         }
+        return stables;
     }
 }
--- src/main/java/mcfroilan/Main.java
+++ src/main/java/mcfroilan/Main.java
@@ -1,28 +1,31 @@
 package mcfroilan;
 
-import java.util.Arrays;
+
 
 public class Main{
     public static void main(String[] args) {
-        Farm farm = new Farm();
-       Chicken[][] chickenCoops = farm.makeChickens();
-        //System.out.println(Arrays.deepToString(farm.makeChickens()));
-        Chicken chicken1 = new Chicken();
-        Chicken chicken2 = new Chicken();
-        Chicken chicken3 = new Chicken();
-        System.out.println(chicken1.getId());
-        System.out.println(chicken2.getId());
-        System.out.println(chicken3.getId());
-        for(Chicken[] chickenCoop: chickenCoops) {
-            for(Chicken chicken: chickenCoop){
-                if(chicken != null && chicken.getId() == 13){
-                    chicken.makeNoise();
-                }
-            }
-        }
-        Farm horses = new Farm();
-        horses.makeHorses(10,3);
-
+//        Farm farm = new Farm();
+//       Chicken[][] chickenCoops = farm.makeChickens();
+//        //System.out.println(Arrays.deepToString(farm.makeChickens()));
+//        Chicken chicken1 = new Chicken();
+//        Chicken chicken2 = new Chicken();
+//        Chicken chicken3 = new Chicken();
+//        System.out.println(chicken1.getId());
+//        System.out.println(chicken2.getId());
+//        System.out.println(chicken3.getId());
+//        for(Chicken[] chickenCoop: chickenCoops) {
+//            for(Chicken chicken: chickenCoop){
+//                if(chicken != null && chicken.getId() == 13){
+//                    chicken.makeNoise();
+//                }
+//            }
+//        }
+        Farm ponies = new Farm();
+        Horse[][] stables = ponies.makeHorses();
+        Horse horse1 = new Horse();
+        Horse horse2 = new Horse();
+        System.out.println(horse1.getId());
+        System.out.println(horse2.getId());
 
 
     }
