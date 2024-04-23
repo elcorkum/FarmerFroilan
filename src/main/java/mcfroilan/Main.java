2d0f07119cb8f3b8864d02786e54d08186e2b1dd
elcorkum <lizzycorkum@gmail.com>
2024-04-22 20:23:31 -0400

    Update CropDuster operate() method

--- src/main/java/mcfroilan/Main.java
+++ src/main/java/mcfroilan/Main.java
@@ -4,39 +4,6 @@ import java.util.Arrays;
 
 public class Main{
     public static void main(String[] args) {
-//        Farmer tim = new Farmer();
-//        CropRow corn = new CropRow(3);
-//        CornStalk cornStalk = new CornStalk();
-//        tim.plant(cornStalk, corn, -1);
-//        System.out.println(Arrays.toString(corn.getCrops()));
-//        Farm farm = new Farm();
-//        Chicken[][] chickenCoops = farm.makeChickens();
-//        Tractor tractor = new Tractor();
-//        Horse horse = new Horse();
-//        CropDuster cropDuster = new CropDuster();
-//        tim.mount(tractor);
-//        //tim.mount(tractor);
-//        //tim.mount(horse);
-//
-////System.out.println(Arrays.deepToString(farm.makeChickens()));
-//        Chicken chicken1 = new Chicken();
-//        Chicken chicken2 = new Chicken();
-//        Chicken chicken3 = new Chicken();
-//        System.out.println(chicken1.getId());
-//        System.out.println(chicken2.getId());
-//        System.out.println(chicken3.getId());
-//        farm.makeChickens();
-//        for(Chicken[] chickenCoop: chickenCoops) {
-//            for(Chicken chicken: chickenCoop){
-//                if(chicken != null && chicken.getId() == 13){
-//                    chicken.makeNoise();
-//                }
-//            }
-//        }
-//
-//        Pilot froilanda = new Pilot();
-//        froilanda.mount(cropDuster);
-
     }
 
 }
